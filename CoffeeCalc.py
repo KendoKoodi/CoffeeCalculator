6ea13c3b35606b78315d8ca55b2447e76425477c
mikavirtala <mika.1.virtala@edu.karelia.fi>
2022-01-06 16:15:56 +0200

    Started with Tkinter code

---
+++ CoffeeCalc.py
@@ -0,0 +1,33 @@
+from tkinter import *
+from tkinter import ttk
+
+def testCommand():
+    pass
+
+root = Tk()
+root.title("Coffee calculator")
+
+mainframe = ttk.Frame ( root, padding="3 3 12 12" )
+mainframe.grid ( column=0, row=0, sticky=( N, W, E, S ) )
+root.columnconfigure ( 0, weight=1 )
+root.rowconfigure ( 0, weight=1 )
+
+userEntry = ttk.Entry ( mainframe, width=7, textvariable="userEntry" )
+userEntry.grid ( column=2, row=1, sticky=( W, E) )
+
+testVar="testVar"
+ttk.Label ( mainframe, textvariable=testVar).grid ( column=2, row=2, sticky=( W, E) )
+
+ttk.Button ( mainframe, text="testButton", command=testCommand ).grid ( column=3, row=3, sticky=W )
+
+ttk.Label ( mainframe, text="userEntry" ).grid( column=3, row=1, sticky=W )
+ttk.Label ( mainframe, text="testVar" ).grid( column=1, row=2, sticky=E )
+ttk.Label ( mainframe, text="testVar2" ).grid( column=3, row=2, sticky=W )
+
+for child in mainframe.winfo_children():
+    child.grid_configure ( padx=5, pady=5 )
+
+userEntry.focus()
+root.bind("<Return>", testCommand)
+
+root.mainloop()
